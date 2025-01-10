7736d3be7ed1c8c5ed75ff313954afd9a02adae1
pre-coder <79780378+pre-coder@users.noreply.github.com>
2025-01-10 09:26:33 +0530

    Challenges given during CurriculumAssociates Interview

---
+++ FindAnagramPairs.java
@@ -0,0 +1,70 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindAnagramPairs {
+
+    public static void main(String[] args) {
+        List<ArrayList<String>> anagramPairsOld = getAnagramPairs(new ArrayList<>(Arrays.asList("eat", "tea", "tan", "ate", "nat", "bat")));
+        System.out.println(anagramPairsOld);
+
+        List<List<String>> anagramPairsNew = getAnagramPairsNew(new ArrayList<>(Arrays.asList("eat", "tea", "tan", "ate", "nat", "bat")));
+        System.out.println(anagramPairsNew);
+    }
+
+    /*
+     * Given an array of strings strs, group the anagrams together. 
+     * You can return the answer in any order.
+     *
+     * Example 1:
+     *
+     * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+     *
+     * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+     */
+    @SuppressWarnings("unchecked")
+    static List<ArrayList<String>> getAnagramPairs(List<String> inputStrs) {
+        List<ArrayList<String>> anagramPairs = new ArrayList<>();
+        ArrayList<String> pair = new ArrayList<>();
+        List<String> skipStrs = new ArrayList<>();
+        for (int i = 0; i < inputStrs.size(); i++) {
+            pair.clear();
+            pair.add(inputStrs.get(i));
+            if (!skipStrs.contains(inputStrs.get(i))) {
+                for (int j = i + 1; j < inputStrs.size(); j++) {
+                    if (isAnagramPair(inputStrs.get(i), inputStrs.get(j))) {
+                        pair.add(inputStrs.get(j));
+                        skipStrs.add(inputStrs.get(j));
+                    }
+                }
+                anagramPairs.add((ArrayList<String>) pair.clone());
+            }
+        }
+        return anagramPairs;
+    }
+
+    static boolean isAnagramPair(String str1, String str2) {
+        char[] str1CharArr = str1.toCharArray();
+        char[] str2CharArr = str2.toCharArray();
+        Arrays.sort(str1CharArr);
+        Arrays.sort(str2CharArr);
+        return new String(str1CharArr).equals(new String(str2CharArr));
+    }
+
+    static List<List<String>> getAnagramPairsNew(List<String> inputStrs) {     
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        inputStrs.forEach(str -> {
+            String sortedStr = str.chars()
+                                .sorted()
+                                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                                .toString();
+            anagramMap.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+        });
+        
+        return new ArrayList<>(anagramMap.values());
+    }
+}
---
+++ FindLeader.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class FindLeader {
+
+    public static void main(String[] args) {
+        List<Integer> leadersOld = findLeaderOld(new int[]{16, 17, 4, 3, 5, 2, 7});
+        System.out.println(leadersOld.toString());
+        List<Integer> leadersNew = findLeaderNew(new int[]{16, 17, 4, 3, 5, 2, 7});
+        System.out.println(leadersNew.toString());
+    }
+
+    /**
+     * Given an array arr[] of size n, the task is to find all the Leaders in
+     * the array. An element is a Leader if it is greater than or equal to all
+     * the elements to its right side.
+     *
+     * Note: The rightmost element is always a leader.
+     *
+     * Examples: Input: arr[] = [16, 17, 4, 3, 5, 2] Output: [17 5 2]
+     *
+     * Explanation: 17 is greater than all the elements to its right i.e., [4,
+     * 3, 5, 2], therefore 17 is a leader. 5 is greater than all the elements to
+     * its right i.e., [2], therefore 5 is a leader. 2 has no element to its
+     * right, therefore 2 is a leader.
+     */
+    static List<Integer> findLeaderOld(int[] numbers) {
+        List<Integer> leaders = new ArrayList<> ();
+        boolean leader;
+        for (int i = 0; i < numbers.length; i++) {
+            leader = true;
+            for (int j = i + 1; j < numbers.length; j++) {
+                leader &= (numbers[i] > numbers[j]);
+            }
+            if (leader) {
+               leaders.add(numbers[i]);
+            }
+        }
+        return leaders;
+    }
+
+    static List<Integer> findLeaderNew(int[] numbers) {
+        List<Integer> leaders = new ArrayList<>();
+        IntStream.range(0, numbers.length).forEach(i -> {
+            if(IntStream.range(i+1, numbers.length).allMatch(j -> numbers[i] > numbers[j])) {
+                leaders.add(numbers[i]);
+            }
+        });
+        return leaders;
+    }
+}
