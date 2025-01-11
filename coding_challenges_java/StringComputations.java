085338aeb18c1751e81c697806a51f15e8e460d7
pre-coder <79780378+pre-coder@users.noreply.github.com>
2025-01-11 17:14:18 +0530

    Count Words

--- coding_challenges_java/StringComputations.java
+++ coding_challenges_java/StringComputations.java
@@ -1,5 +1,6 @@
 package coding_challenges_java;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -19,6 +20,26 @@ public class StringComputations {
 
         /* swap characters in strings at specific positions. */
         System.out.println(swapCharacters("Hello", 1, 4));
+
+        /* Write programs to count repeated words in a sentence. */
+        System.out.println(countWords("Given an array of integers arr, return true if and only if it is a valid mountain array."));
+        System.out.println(countWords("a set of words that is complete in itself, typically containing a subject and predicate, conveying a statement, question, exclamation, or command, and consisting of a main clause and sometimes one or more subordinate clauses."));
+    }
+
+    static Integer countWords(String sentence) {
+        String cleanedSentence = sentence.replace("[^a-zA-Z0-9 ]", "");
+        String[] splitSentence = cleanedSentence.split("\\s+");
+        TreeMap<String, Integer> countWordsMap = new TreeMap<>();
+        for (String word : splitSentence) {
+            countWordsMap.merge(word, 1, Integer::sum);
+        }
+        int count = 0;
+        for (String word : countWordsMap.keySet()) {
+            if (countWordsMap.get(word) > 1) {
+                count++;
+            }
+        }
+        return count;
     }
 
     static String swapCharacters(String str, int fromPosition, int toPosition) {
@@ -31,11 +52,11 @@ public class StringComputations {
 
     static void stringCompressionUsingCount(String str) {
         Map<Character, Integer> compressionCount = new TreeMap<>();
-        for (char c: str.toCharArray()) {
+        for (char c : str.toCharArray()) {
             compressionCount.merge((char) c, 1, Integer::sum);
-        }  
+        }
         for (Map.Entry<Character, Integer> en : compressionCount.entrySet()) {
-            System.out.print(en.getKey() + "" + en.getValue()); 
+            System.out.print(en.getKey() + "" + en.getValue());
         }
         System.out.println();
     }
