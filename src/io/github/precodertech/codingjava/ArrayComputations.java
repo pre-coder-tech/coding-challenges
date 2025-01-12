4e7dddb1e7c0f48d6fbad84c2bd27034a77f406f
pre-coder <79780378+pre-coder@users.noreply.github.com>
2025-01-12 14:36:16 +0530

    program to increment last digit in an array

--- src/io/github/precodertech/codingjava/ArrayComputations.java
+++ src/io/github/precodertech/codingjava/ArrayComputations.java
@@ -1,7 +1,18 @@
 package io.github.precodertech.codingjava;
 
+import java.util.Arrays;
+
 public class ArrayComputations {
     public static void main(String[] args) {
         /** program to increment last digit in an array */
+        int[] arr = new int[] {1,2,4,8,20,6,9,24,7,3,7,2,457,5,234};
+        increamentLastDigit(arr, 2);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void increamentLastDigit(int[] arr, int k) {
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] + k;
+        }
     }
 }
