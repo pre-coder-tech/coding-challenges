160b809076b91e3d716cc7633970332be4254b4d
pre-coder <79780378+pre-coder@users.noreply.github.com>
2025-01-12 14:09:21 +0530

    package hotfix

---
+++ src/io/github/precodertech/codingjava/ArrayComputations.java
@@ -0,0 +1,7 @@
+package io.github.precodertech.codingjava;
+
+public class ArrayComputations {
+    public static void main(String[] args) {
+        /** program to increment last digit in an array */
+    }
+}
---
+++ src/io/github/precodertech/codingjava/CommonWords.java
@@ -0,0 +1,25 @@
+package io.github.precodertech.codingjava;
+
+public class CommonWords {
+
+    public static void main(String[] args) {
+
+        /**
+         * Given a string paragraph and a string array of the banned words
+         * banned, return the most frequent word that is not banned. It is
+         * guaranteed there is at least one word that is not banned, and that
+         * the answer is unique.
+         *
+         *          *The words in paragraph are case-insensitive and the answer should
+         * be returned in lowercase.
+         */
+        System.out.println(mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new String[]{"hit"}));
+
+    }
+
+    static String mostCommonWord(String paragraph, String[] banned) {
+        return "";
+
+    }
+
+}
